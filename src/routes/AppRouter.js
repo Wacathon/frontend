fabb7810108d69924e5e556295831cd3e826018d
Seokyung <xuejd17@gmail.com>
2023-08-19 04:35:25 +0900

    Merged

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -7,15 +7,10 @@ import LoginPage from "../pages/LoginPage";
 import LinkPage from "../pages/LinkPage";
 
 import "./route.css";
-<<<<<<< HEAD
-import LinkPage from "../pages/LinkPage";
 import FeedbackFormPage from "../pages/FeedbackFormPage";
-=======
->>>>>>> e851c461ef7470ac945f0e0f2868821eee5bfc09
 
 function AppRouter({ isLoggedIn }) {
 	return (
-<<<<<<< HEAD
 		<>
 			{isLoggedIn ? (
 				<div className="d-flex flex-row">
@@ -34,30 +29,15 @@ function AppRouter({ isLoggedIn }) {
 				<div className="d-flex justify-content-center">
 					<div className="guest-main-wrapper">
 						<Routes>
-							{/* <Route path="/" element={<LoginPage />} /> */}
-							<Route path="/" element={<FeedbackFormPage />} />
-							{/* <Route path="/feedback-form" element={<FeedbackFormPage />} /> */}
+							<Route path="/" element={<LoginPage />} />
+							{/* <Route path="/" element={<FeedbackFormPage />} /> */}
+							<Route path="/feedback-form" element={<FeedbackFormPage />} />
 							<Route path="/share-link" element={<LinkPage />} />
 						</Routes>
 					</div>
 				</div>
 			)}
 		</>
-=======
-		<div className="d-flex flex-row">
-			<div className="nav-wrapper">
-				<Navigation />
-			</div>
-			<div className="main-wrapper">
-				<Routes>
-					<Route path="/" element={<CardPage />} />
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/mypage" element={<MyPage />} />
-					<Route path="/share-link" element={<LinkPage />} />
-				</Routes>
-			</div>
-		</div>
->>>>>>> e851c461ef7470ac945f0e0f2868821eee5bfc09
 	);
 }
 
