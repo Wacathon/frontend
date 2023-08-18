e90cf74a355bb18c2adc4263c418453a191ed265
Seokyung <xuejd17@gmail.com>
2023-08-19 02:34:47 +0900

    My Page My HexChart

--- src/components/mypages/MyInfoForm.js
+++ src/components/mypages/MyInfoForm.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { userInfo } from "../cards/UserInfo";
 import { Button, Stack } from "react-bootstrap";
 
 function MyInfoForm() {
+	const [isEdit, setIsEdit] = useState(false);
+
+	const onEditClick = () => {
+		setIsEdit((prev) => !prev);
+	};
+
 	return (
 		<Stack gap={4} className="userForm-container">
-			<h5>이름: {userInfo.name}</h5>
-			<h5>소개: {userInfo.intro}</h5>
-			<h5>연락처: {userInfo.tel}</h5>
-			<h5>이메일: {userInfo.email}</h5>
+			{isEdit ? (
+				<></>
+			) : (
+				<>
+					<h5>이름: {userInfo.name}</h5>
+					<h5>소개: {userInfo.intro}</h5>
+					<h5>연락처: {userInfo.tel}</h5>
+					<h5>이메일: {userInfo.email}</h5>
+				</>
+			)}
 			<div className="d-flex justify-content-end">
-				<Button>수정</Button>
+				<Button onClick={onEditClick}>수정</Button>
 			</div>
 		</Stack>
 	);
